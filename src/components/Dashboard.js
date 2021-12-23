3e8a1cba44765d0ec2b196fdebcc02f1a80f55f5
Mohamed Shahul Hameed <thameemsh@email.com>
2021-12-22 19:28:11 -0500

    Add Webscoket connection for live updates

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -8,30 +8,30 @@ import {
   getTotalInterviews,
   getLeastPopularTimeSlot,
   getMostPopularDay,
-  getInterviewsPerDay
- } from "helpers/selectors";
+  getInterviewsPerDay,
+} from "helpers/selectors";
+import { setInterview } from "helpers/reducers";
 
- 
 const data = [
   {
     id: 1,
     label: "Total Interviews",
-    getValue: getTotalInterviews
+    getValue: getTotalInterviews,
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    getValue: getLeastPopularTimeSlot
+    getValue: getLeastPopularTimeSlot,
   },
   {
     id: 3,
     label: "Most Popular Day",
-    getValue: getMostPopularDay
+    getValue: getMostPopularDay,
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    getValue: getInterviewsPerDay
+    getValue: getInterviewsPerDay,
   },
 ];
 
@@ -54,15 +54,27 @@ class Dashboard extends Component {
     Promise.all([
       axios.get("/api/days"),
       axios.get("/api/appointments"),
-      axios.get("/api/interviewers")
+      axios.get("/api/interviewers"),
     ]).then(([days, appointments, interviewers]) => {
       this.setState({
         loading: false,
         days: days.data,
         appointments: appointments.data,
-        interviewers: interviewers.data
+        interviewers: interviewers.data,
       });
     });
+
+    this.socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+
+    this.socket.onmessage = event => {
+      const data = JSON.parse(event.data);
+    
+      if (typeof data === "object" && data.type === "SET_INTERVIEW") {
+        this.setState(previousState =>
+          setInterview(previousState, data.id, data.interview)
+        );
+      }
+    };
   }
 
   componentDidUpdate(previousProps, previousState) {
@@ -71,14 +83,17 @@ class Dashboard extends Component {
     }
   }
 
+  componentWillUnmount() {
+    this.socket.close();
+  }
+
   selectPanel(id) {
     this.setState((previousState) => ({
-      focused: previousState.focused !== null ? null : id
+      focused: previousState.focused !== null ? null : id,
     }));
   }
 
   render() {
-
     const dashboardClasses = classnames("dashboard", {
       "dashboard--focused": this.state.focused,
     });
